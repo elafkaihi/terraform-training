a9a2e1f948d0e361736f6988856e178242c92be7
elafkaihi <elmehdi.lafkaihi@centrale-casablanca.ma>
2024-06-29 17:44:09 +0100

    terraform fmt

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.AWS_REGION
+  region     = var.AWS_REGION
   access_key = "your-access-key"
   secret_key = "your-secret-key"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,21 +1,21 @@
 module "vpc" {
-    source = "terraform-aws-modules/vpc/aws"
-    name = var.VPC_NAME
-    cidr = var.VPC_CIDR
-    azs = [var.Zone1, var.Zone2, var.Zone3]
-    private_subnets = [var.PrivSub1_CIDR, var.PrivSub2_CIDR, var.PrivSub3_CIDR]
-    public_subnets = [var.PubSub1_CIDR, var.PubSub2_CIDR, var.PubSub3_CIDR]
-    enable_nat_gateway = true
-    enable_vpn_gateway = true
-    enable_dns_hostnames = true
-    enable_dns_support   = true
+  source               = "terraform-aws-modules/vpc/aws"
+  name                 = var.VPC_NAME
+  cidr                 = var.VPC_CIDR
+  azs                  = [var.Zone1, var.Zone2, var.Zone3]
+  private_subnets      = [var.PrivSub1_CIDR, var.PrivSub2_CIDR, var.PrivSub3_CIDR]
+  public_subnets       = [var.PubSub1_CIDR, var.PubSub2_CIDR, var.PubSub3_CIDR]
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Terraform = "true"
-        Environment = "dev"
-    }
-    vpc_tags = {
-        Name = var.VPC_NAME
-    }
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+  vpc_tags = {
+    Name = var.VPC_NAME
+  }
 
 }
